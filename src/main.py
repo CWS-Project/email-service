05c9195e462ccdf8debc5b87ce2b86e2bf067e68
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-04-16 12:46:33 +0530

    Health Check fixed

--- src/main.py
+++ src/main.py
@@ -8,3 +8,7 @@ app.include_router(mail_router)
 @app.get("/healthz")
 def health_check(response: Response):
     return make_response(response, 200, "OK")
+
+@app.get("/")
+def health_check(response: Response):
+    return make_response(response, 200, "OK")
